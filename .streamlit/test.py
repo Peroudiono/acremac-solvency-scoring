4330f397d67b429da5433d0dfcafefbb42732ac1
Péroudiono <dionoditboubacarperou@gmail.com>
2026-07-12 04:21:06 +0100

    Nettoyage du projet

--- .streamlit/test.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-
-print("admin :", hashlib.sha256("acremac2025".encode()).hexdigest())
-print("analyste :", hashlib.sha256("analyste123".encode()).hexdigest())
-print("boubacar :", hashlib.sha256("perou2025".encode()).hexdigest())
